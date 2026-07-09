db5821390d0f4b6baa32b38fafc27f26cabe9727
Harsh Singh <harshsingh25665@gmail.com>
2026-07-09 10:58:41 +0530

    Refactor MongoDB collection variable assignments
    Refactored MongoDB collection variable assignments for clarity.

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -1,5 +1,4 @@
 import os
-
 from pymongo import MongoClient
 
 MONGO_URI = os.getenv("MONGO_URI")
@@ -8,7 +7,10 @@ client = MongoClient(MONGO_URI)
 
 db = client["atlas"]
 
-positions_collection = db["positions"]
-trades_collection = db["trades"]
-run_lock_collection = db["run_lock"]
-settings_collection = db["settings"]
+# Collections
+positions = db["positions"]
+trades = db["trades"]
+run_lock = db["run_lock"]
+settings = db["settings"]
+
+print("✅ MongoDB Connected")
