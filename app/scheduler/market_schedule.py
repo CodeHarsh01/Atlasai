e96108f17299cfced4b90801780b8148660e6ddd
Harsh Singh <harshsingh25665@gmail.com>
2026-07-21 10:13:01 +0530

    Simplify should_run function to always return True
    Removed time-based conditions for scheduling.

--- app/scheduler/market_schedule.py
+++ app/scheduler/market_schedule.py
@@ -3,19 +3,5 @@ from zoneinfo import ZoneInfo
 
 
 def should_run():
-
-    now = datetime.now(
-        ZoneInfo("Asia/Kolkata")
-    )
-
-    minutes = now.hour * 60 + now.minute
-
-    # Morning: 9:15 - 9:44
-    if 9 * 60 + 15 <= minutes < 9 * 60 + 45:
-        return True
-
-    # Afternoon: 3:30 - 3:59
-    if 15 * 60 + 30 <= minutes < 16 * 60:
-        return True
-
-    return False
+    
+    return True
